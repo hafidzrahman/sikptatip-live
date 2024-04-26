d669b4e34c272b4d32198c75d1531da4e8dd208c
hafidzrahman <hafidzrahman559@gmail.com>
2024-04-26 23:34:30 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	deleted:    app/api/account/route.js
    	modified:   lib/actions.js

--- app/api/account/route.js
+++
@@ -1,9 +0,0 @@
-import fs from 'node:fs';
-import path from 'node:path'
-
-export async function GET() {
-    const filePath = path.join(process.cwd(), 'db', 'accounts.json');
-    const readedFile = fs.readFileSync(filePath);
-    const accountData = JSON.parse(readedFile);
-    return Response.json(accountData);
-}
